e7d8529cd7a19c45be0c073b7bf96bfe37303523
Aananth V <aananthv@outlook.com>
2019-10-14 21:06:33 +0530

    Make page login only

--- app/routes/inductions.js
+++ app/routes/inductions.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const router = express.Router()
 const inductionsController = require('../controllers/inductionsController.js')
+const studentAuthController = require('../controllers/studentAuthController.js')
 
-router.get('/', inductionsController.show)
+router.get('/', studentAuthController.checkStudentLogin, inductionsController.show)
 
 module.exports = router
