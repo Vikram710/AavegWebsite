ac4578bbae7606704d6cc6b710cceaa6c1c56930
Aananth V <aananthv@outlook.com>
2019-10-14 19:37:24 +0530

    Inductions page resign, split files

---
+++ public/js/inductions.js
@@ -0,0 +1,27 @@
+function doc(event){
+    document.getElementById('hameed').style.display="none";
+    if(event.target.id =="oc"){
+        document.getElementById('oc').classList.add('selected');
+        document.getElementById('content').classList.remove('selected');
+        document.getElementById('design').classList.remove('selected');
+        document.getElementById('oc-doc').style.display="block";
+        document.getElementById('content-doc').style.display="none";
+        document.getElementById('design-doc').style.display="none";
+    }
+    if(event.target.id =="content"){
+        document.getElementById('oc').classList.remove('selected');
+        document.getElementById('content').classList.add('selected');
+        document.getElementById('design').classList.remove('selected');
+        document.getElementById('content-doc').style.display="block";
+        document.getElementById('oc-doc').style.display="none";
+        document.getElementById('design-doc').style.display="none";
+    }
+    if(event.target.id =="design"){
+        document.getElementById('oc').classList.remove('selected');
+        document.getElementById('content').classList.remove('selected');
+        document.getElementById('design').classList.add('selected');
+        document.getElementById('design-doc').style.display="block";
+        document.getElementById('oc-doc').style.display="none";
+        document.getElementById('content-doc').style.display="none";
+    }
+}
