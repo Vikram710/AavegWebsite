05ec64a8d4ac3e75ac14abd94d119eacb1ea67a5
vikram710 <vikram710v@gmail.com>
2019-12-27 23:35:21 +0530

    Allow cors

--- app.js
+++ app.js
@@ -56,6 +56,11 @@ app.get('/', (req, res) => {
 
 // app.use(tshirtRoutes)
 // app.use(photographyRoutes)
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
 app.use(studentAuthRoutes)
 app.use(adminAuthRoutes)
 app.use(hostelRoutes)
