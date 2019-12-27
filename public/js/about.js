a0e5cfcd605dca26bbc75bb146bfd15f9105a4a7
vikram710 <vikram710v@gmail.com>
2019-12-28 00:51:35 +0530

    API

--- public/js/about.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-  $('.carouselContainer').slick({
-    autoplay: true,
-    autoplaySpeed: 3000,
-    arrows: false,
-    adaptiveHeight: true,
-    dots: true
-  })
-})
